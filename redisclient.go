5a4ce33b6a73bb9a3476cf136205d9773f65478f
gaoxiaoguang <gaoxiaoguang@rongcloud.cn>
2016-08-03 15:18:42 +0800

    add mget/mset

--- redisclient.go
+++ redisclient.go
@@ -50,7 +50,7 @@ func (self *RedisClient)Do(cmd string,args ...interface{})(interface{},error){
 //Ping
 func (self *RedisClient) Ping()(string,error){
 	ret,err := self.handleMethod("PING")
-	if ret != nil {
+	if ret != nil  && err==nil{
 		return ret.(string),err
 	}
 	return "FAIL", err
@@ -59,7 +59,7 @@ func (self *RedisClient) Ping()(string,error){
 //Get key
 func (self *RedisClient) Get(key string)([]uint8,error){
 	ret,err := self.handleMethod("GET",key)
-	if ret != nil{
+	if ret != nil && err==nil{
 		return ret.([]uint8),err
 	}
 	return nil , err
@@ -67,7 +67,7 @@ func (self *RedisClient) Get(key string)([]uint8,error){
 //Set key value
 func (self *RedisClient) Set(key string, value interface{})(string,error){
 	ret,err := self.handleMethod("SET", key, value)
-	if ret != nil{
+	if ret != nil && err==nil{
 		return ret.(string),err
 	}
 	return "FAIL", err
@@ -75,7 +75,7 @@ func (self *RedisClient) Set(key string, value interface{})(string,error){
 //Quit
 func (self *RedisClient) Quit()(string,error){
 	ret,err := self.handleMethod("QUIT")
-	if ret != nil{
+	if ret != nil && err==nil{
 		return ret.(string),err
 	}
 	return "FAIL", err
@@ -83,8 +83,29 @@ func (self *RedisClient) Quit()(string,error){
 //Exists key
 func (self *RedisClient) Exists(key string)(bool,error){
 	ret,err := self.handleMethod("EXISTS", key)
-	if ret != nil{
+	if ret != nil && err==nil{
 		return ret.(int64)>0,err
 	}
 	return false,err
+}
+
+//mget
+func (self *RedisClient)MGet(keys ...interface{})([][]uint8,error){
+	ret,err := self.handleMethod("MGET",keys...)
+	if ret != nil&&err==nil{
+		var arr [][]uint8
+		for _,v := range ret.([]interface{}){
+			arr = append(arr, v.([]uint8))
+		}
+		return arr,nil
+	}
+	return nil,err
+}
+//mset
+func (self *RedisClient)MSet(keyVals ...interface{})(string,error){
+	ret,err := self.handleMethod("MSET", keyVals...)
+	if err==nil&&ret!=nil{
+		return ret.(string),err
+	}
+	return "",err
 }
